8dc9cacd2a896b6276f16304c2b35e55a4e31645
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-23 16:57:43 -0700

    add show method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
         format.js { render partial: 'friends/result' }
       end
     end
-    
+
     def add_friend
       @friend = User.find(params[:friend])
       current_user.friendships.build(friend_id: @friend.id)
@@ -31,5 +31,10 @@ class UsersController < ApplicationController
         flash[:danger] = "There was something wrong with the friend request"
       end  
       redirect_to my_friends_path
+    end
+
+    def show
+      @user = User.find(params[:id])
+      @user_stocks = @user.stocks
     end
 end
