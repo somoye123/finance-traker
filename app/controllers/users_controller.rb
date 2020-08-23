181137561ba69c112b85f2996fdefe4747f94d6c
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-22 23:57:00 -0700

    add search method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
     
     def my_portfolio
-        @user_stocks = current_user.stocks
-        @user = current_user
+      @user_stocks = current_user.stocks
+      @user = current_user
     end
 
     def my_friends
-        @friendships = current_user.friends
+      @friendships = current_user.friends
     end
     
+    def search
+      if params[:search_param].blank?
+        flash.now[:danger] = "You have entered an empty search string"
+      else
+        @users = User.search(params[:search_param])
+        @users = current_user.except_current_user(@users)
+        flash.now[:danger] = "No users match this search criteria" if @users.blank?
+      end
+      respond_to do |format|
+        format.js { render partial: 'friends/result' }
+      end
+    end
 end
