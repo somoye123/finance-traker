df0a99286a11b690e70d027f8569e9ee47105b18
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-22 22:54:13 -0700

    add routing for searching friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: 'users#my_friends'
-  resources :users, only: [:show]
-  resources :friendships
+  get 'search_friends', to: 'users#search'
 end
